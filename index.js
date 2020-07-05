ac59a1bb8c055a5ec30095c994962b38611ed71a
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-07-05 12:10:09 +0700

    add app utility

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const bim = require('./lib/bim')
 const database = require('./lib/database')
+const server = require('./lib/server')
 const view = require('./lib/server/view')
 
 module.exports = {
   bim,
   Model: database.Model,
   View: view.View,
+  app: server.app,
 };
