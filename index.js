b303836cfce2319fb7e73f49456edba13255d51d
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-07-05 10:43:00 +0700

    create view base class

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const bim = require('./lib/bim')
 const database = require('./lib/database')
+const view = require('./lib/server/view')
 
 module.exports = {
   bim,
   Model: database.Model,
+  View: view.View,
 };
